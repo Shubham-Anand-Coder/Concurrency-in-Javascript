59a1970d434352aa21ce8dcf85542f6ba6579dc4
shubham <futurepm123@gmail.com>
2024-10-07 10:34:17 +0530

    Removed cooments

--- callback.js
+++ callback.js
@@ -1,22 +1,22 @@
-// JS main thread does not waits
+// ---- JS main thread does not waits ---------------------------------------
 
-// function doSomething(name, count) {
-//     let cnt = 0;
-//     let x = setInterval(function() {
-//         cnt++;
-//         console.log("Printing name - ", name);
-//         if(cnt == count) {
-//             clearInterval(x);
-//         } 
-//     }, 2000)
-// }
-
-
-// doSomething('Shubham', 3);
-// doSomething('Aryan', 2);
+function doSomething(name, count) {
+    let cnt = 0;
+    let x = setInterval(function() {
+        cnt++;
+        console.log("Printing name - ", name);
+        if(cnt == count) {
+            clearInterval(x);
+        } 
+    }, 2000)
+}
 
 
-// In order to print names sequentially we can use callback methods instead
+doSomething('Shubham', 3);
+doSomething('Aryan', 2);
+
+
+// ---- In order to print names sequentially we can use callback methods instead -----------
 
 function doSomethingWithCallback(name, count, callbackMethod) {
     let cnt = 0;
