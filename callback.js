28f86b148e0c4b148a64271b7a0e7266332e2be2
shubham <futurepm123@gmail.com>
2024-10-07 10:33:07 +0530

    Added callback function to run it sequentially

--- callback.js
+++ callback.js
@@ -1,14 +1,37 @@
-function doSomething(name, count) {
+// JS main thread does not waits
+
+// function doSomething(name, count) {
+//     let cnt = 0;
+//     let x = setInterval(function() {
+//         cnt++;
+//         console.log("Printing name - ", name);
+//         if(cnt == count) {
+//             clearInterval(x);
+//         } 
+//     }, 2000)
+// }
+
+
+// doSomething('Shubham', 3);
+// doSomething('Aryan', 2);
+
+
+// In order to print names sequentially we can use callback methods instead
+
+function doSomethingWithCallback(name, count, callbackMethod) {
     let cnt = 0;
     let x = setInterval(function() {
         cnt++;
         console.log("Printing name - ", name);
         if(cnt == count) {
+            callbackMethod();
             clearInterval(x);
         } 
     }, 2000)
 }
 
 
-doSomething('Shubham', 3);
-doSomething('Aryan', 2);
+doSomethingWithCallback('Shubham', 3, () => {
+    doSomethingWithCallback('Aryan', 2, () => {
+    }); 
+});
